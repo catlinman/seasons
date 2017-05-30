1d6c9f6eb8bfa0dee600ae4db0993e5587919996
catlinman <contact@catlinman.com>
2017-05-31 00:42:00 +0200

    Implemented asset loading and started building the scene

--- scene.js
+++ scene.js
@@ -6,6 +6,7 @@ var canvas;
 // Scene definitions.
 var sceneWidth;
 var sceneHeight;
+var sceneReveal;
 
 // Color definitions.
 var colorBackground;
@@ -14,6 +15,9 @@ var colorBackground;
 var cursorVector; // Positional vector of the cursor.
 var cursorSize; // Current radius value of the cursor.
 var cursorSizeDefault; // Default radius value of the cursor.
+
+// Preload assets. These are instantiated in the preload function.
+var treeImage;
 
 // Draw a circle at the mouse cursor.
 function drawCursor() {
@@ -26,6 +30,11 @@ function drawCursor() {
     // Draw an ellipse at the cursor vector position.
     fill(255, 255, 255);
     ellipse(cursorVector.x, cursorVector.y, cursorSize);
+}
+
+// Preload assets before the scene starts.
+function preload() {
+    treeImage = loadImage("assets/tree.svg");
 }
 
 // Program entry point.
@@ -41,9 +50,13 @@ function setup() {
     noStroke(); // Disable strokes by default.
     noCursor(); // Disable the default mouse cursor.
 
+    // Make sure that images are drawn from their center origin.
+    imageMode(CENTER);
+
     // Set variables for the scene.
     sceneWidth = 640;
     sceneHeight = 640;
+    sceneReveal = 255;
 
     // Set the default mouse position to the center of the drawing scene.
     mouseX = width / 2;
@@ -54,7 +67,7 @@ function setup() {
 
     // Set the correct values for the cursor size.
     cursorSizeDefault = 16;
-    cursorSize = cursorSizeDefault;
+    cursorSize = 0;
 
     // Set color variables.
     colorBackground = color(48, 35, 69);
@@ -80,11 +93,14 @@ function mousePressed() {
 }
 
 function drawSun(x, y, r, num, c) {
+    // Separate color channels for easier use.
     var cr = red(c);
     var cg = green(c);
     var cb = blue(c);
 
+    // Draw iterations of ellipses with different opacity.
     for (i = 1; i < num + 1; i++) {
+        // Set the fill color for the given ellipse.
         fill(
             cr,
             cg,
@@ -92,12 +108,12 @@ function drawSun(x, y, r, num, c) {
             255 - (255 / num) * i - 1
         );
 
+        // Draw the actual ellipse and animate its radius.
         ellipse(
             x,
             y,
             ((r / num) * i) + sin(frameCount / 150) * r / (num * 2)
-
-            );
+        );
     }
 }
 
@@ -105,8 +121,29 @@ function drawSun(x, y, r, num, c) {
 function draw() {
     background(colorBackground); // Reset the background color.
 
-    drawSun(width / 2, sceneHeight / 2, 256, 8, color(255, 245, 3));
+    // Draw a sun in the center of the scene.
+    drawSun(width / 2, sceneHeight / 2, 312, 8, color(255, 245, 3));
+
+    // Tint foreground images.
+    tint(48, 35, 69, 235);
+    image(treeImage, width / 2, sceneHeight / 2 + 40, 128, 256);
+
+    // Set the fill for drawn elements.
+    fill(48, 35, 69, 235);
+
+    // Start drawing ellipses.
+    ellipse(width / 2 + 150, sceneHeight / 2 + 64, 64, 86);
+    ellipse(width / 2 - 150, sceneHeight / 2 + 64, 64, 86);
+    ellipse(width / 2 - 120, sceneHeight / 2 + 86, 64);
+    ellipse(width / 2 + 120, sceneHeight / 2 + 86, 48);
 
     // Draw the cursor.
     drawCursor();
+
+    // Draw a veil above everything.
+    fill(48, 35, 69, sceneReveal);
+    rect(0, 0, windowWidth, windowHeight);
+
+    // Remove opacity.
+    sceneReveal--;
 }
